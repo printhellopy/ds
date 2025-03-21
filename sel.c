4c898298ac3df8cd53fd47d0c177d3500212108c
printhellopy <trip04072024@gmail.com>
2025-03-21 06:14:53 +0530

    1.c

--- sel.c
+++ sel.c
@@ -1,29 +1,115 @@
-#include <stdio.h> 
-int main()  
-{ 
- int arr[10]={6,12,0,18,11,99,55,45,34,2}; 
- int n=10; 
- inti, j, position, swap; 
- printf(“Array elements before sorting”); 
- for (i = 0; i< n; i++) 
-  printf("%d\t", arr[i]); 
- for (i = 0; i< (n - 1); i++)  
- { 
-  position = i; 
-  for (j = i + 1; j < n; j++)  
-  { 
-   if (arr[position] >arr[j]) 
-   position = j; 
-  } 
-  if (position != i)  
-  { 
-   swap = arr[i]; 
-   arr[i] = arr[position]; 
-   arr[position] = swap; 
-  } 
- } 
- printf(“Array elements after sorting”); 
- for (i = 0; i< n; i++) 
-  printf("%d\t", arr[i]); 
- return 0; 
+Import java.util.*; 
+class library 
+Protected Storing ISDN, category;
+Void read() 
+{ Scanner s= new Scanner(System.in); 
+ System.out.println(Enter the details of books:);
+ System.out.println("ISBN");
+ 15BN=S.next();
+ System.out.printen (category:");
+ category=s.next()) 
+ Void display() 
+} 
+System.out.println("Details of Books: ");
+System.out.println("ISBN: "+580; 
+System.out.println("Category:" + category);
+}
+}
+Class Bookinfo Extends liberary
+{
+Protected string book name, author, Publishers
+{
+Void read book Info) Void
+{
+Scanner se=new Scanner(System.in);
+
+System.out.println("Enter book name:"
+
+book name-Sconext();
+
+System-out-Perintln("Enter authet name");
+
+author=s.next(),
+
+System.out.println("Enter Publishes: ");
+
+Pub Seinext);
+
+}
+
+Void disp Book InfoC)
+{
+System-out-Printen (Book name: " + backname) ;
+
+System.out.println(" Author names:"+author),
+
+System.out.println(" Publisher: "+ Publishe) ;
+}
+}
+}
+Class Book Price extends BookInfo
+{
+double Perice;
+
+String & Purchase;
+
+Void cread Price)
+{
+ Scanner $1=new Scanner(System.in);
+
+System.out.println("Perice:");
+
+Price=51.next Double();
+
+System.out.println("Date of Purchase:");
+
+d purchase-si.next();
+
+}
+
+Void display Price();
+
+System.out.println("Porice:" + Price); System.out System.out.println(Date
+
+Date of Purchase:"+
+
+d Purchase);
+
+}
+
+Public class Library Info
+
+Public static void main(String args[]) Public
+
+double Lotal Perice;
+
+int nam
+
+Scannel $2=new Scanner(System.in);
+
+System.out.println("Enter the number of books");
+
+Rum. 52.nextInt();
+
+BookPrice[] books = new book Perice (num);
+
+for(i=0;i<num; i++)
+
+book [i] = new Book Price();
+
+books [i] cread book()%
+
+book [i]-read();
+
+books [i].cread Porice();
+books[i].display();
+
+book (i].display Porice(); total Price book [i]. Perice;
+
+System.out.printdn("Total Price of books
+
+:" + total Porpce);
+
+}
+
 }
